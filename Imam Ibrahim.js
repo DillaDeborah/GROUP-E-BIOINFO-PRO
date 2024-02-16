a848fae110d277af8b907ba07740a829ba198038
Khaleeel004 <160235751+Khaleeel004@users.noreply.github.com>
2024-02-16 13:02:50 +0100

    Add files via upload

---
+++ Imam Ibrahim.js
@@ -0,0 +1,10 @@
+name = "Ibrahim Imam Ibrahim"
+email = "imamibrahim259@gmail.com"
+slack_username = "@khaleeel004"
+area_of_interest = "Data analysis"
+
+print("=== My Information ===")
+print("Name:", name)
+print("Email:", email)
+print("Slack username:", slack_username)
+print("Area of interest:", area_of_interest)
