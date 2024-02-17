df1dd34416e821912cd7cf3be0f075aaaa7de298
Khaleeel004 <160235751+Khaleeel004@users.noreply.github.com>
2024-02-17 10:53:28 +0100

    Add files via upload

---
+++ Balkisu assignment.py
@@ -0,0 +1,9 @@
+name = "Balkisu Sulaiman"
+username = "@balkisu"
+email = "balkisusulaiman4524@gmail.com"
+interest = "bioinformatics"
+
+print(f"Name: {name}")
+print(f"Username: {username}")
+print(f"Email: {email}")
+print(f"Interest: {interest} (bioinformatics)")
